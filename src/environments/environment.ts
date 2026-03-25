ba0613193cf0014a635c0a0ff9fafab4edc7aec4
Jorge Chafloque Millones <37223352+jchafloquem@users.noreply.github.com>
2026-03-25 17:28:24 -0500

    Actualizacion al 25-03-2026

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,8 @@ export const environment = {
   production: false,
   // Para EMULADOR usa: http://10.0.2.2:3000/api
   // Para DISPOSITIVO FÍSICO (celular) usa la IP de tu computadora en la red WiFi.
-  apiUrl: 'http://192.168.26.12:3000',
+  //apiUrl: 'http://192.168.26.12:3000',
+  apiUrl: 'https://sistemas.devida.gob.pe/geodaismovil:3000',
   firebase: {
       apiKey: "AIzaSyBxuC9v9dbaQyR2jS9NkBeKeOo86EsGhvY",
       authDomain: "geomovildais.firebaseapp.com",
